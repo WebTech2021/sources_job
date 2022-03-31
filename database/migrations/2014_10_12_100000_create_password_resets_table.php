d615e4c9865008cf0e79679494a734c1ee9f6291
shamimxp <shamim.ahamadxp@gmaile.com>
2022-03-31 18:00:29 +0600

    add password reset table

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -3,8 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
-return new class extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -29,4 +28,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('password_resets');
     }
-};
+}
